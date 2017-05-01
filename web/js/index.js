57b405024d59a3790aeca55e032787b25f427f65
wangxin <14302010064@fudan.edu.cn>
2017-05-02 01:39:18 +0800

    add customer form. add library to parse json and xml.

--- web/js/index.js
+++ web/js/index.js
@@ -8,7 +8,7 @@ app.controller('myCtrl', function($scope) {
     // This part is about loading information of our object
     // a. These information should be loaded from the configure file, e.g xml file
     // b. Then show in the corresponding place in the html page using Angular.js model
-    $scope.Materials = [
+    $scope.Colors = [
         {content : "Red", price : 15, color: 0xff0000},
         {content : "Green", price : 20, color: 0x00ff00},
         {content : "Blue", price : 20, color: 0x0000ff},
@@ -17,10 +17,14 @@ app.controller('myCtrl', function($scope) {
         {content : "Semicircle", price : 2},
         {content : "Triangle", price : 5}
     ];
+    $scope.Sizes = [
+        {size : "25 * 25", price: 5},
+        {size : "50 * 50", price: 8}
+    ];
 
     // Call back to response to the users' choices, 
     // e.g. when the selected Material is change, change the scene at the same time 
-    $scope.$watch('selectedMaterial', function(val){
+    $scope.$watch('selectedColor', function(val){
         console.log(val);
         var obj = eval (val);
         if(obj){
